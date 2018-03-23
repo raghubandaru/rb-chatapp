dac4606da99fdef5cff1e2fa0cbb02c9abb4857a
Raghu <raghubandaru45@gmail.com>
2018-03-23 11:41:32 +0530

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,20 +2,11 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Raghu',
-        text: 'Hi Goutham.'
-    });
 });
 
 socket.on('disconnect', function () {
     console.log('Disconnected from server');
 });
-
-// socket.on('newEmail', function(email) {
-//     console.log('New email', email);
-// });
 
 socket.on('newMessage', function(message) {
     console.log('newMessage', message);
--- server/server.js
+++ server/server.js
@@ -14,14 +14,14 @@ app.use(express.static(path.join(publicPath)));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', {
-        name: 'Goutham',
-        text: 'Hey!',
-        createdAt: 1234
-    });
-
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
+
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
